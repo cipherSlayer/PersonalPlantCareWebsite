dd0f385548c75e9a5d6a170ed65f0d7dc3ff7588
Nils <134720984+cipherSlayer@users.noreply.github.com>
2023-06-26 19:12:08 +0200

    new login page

--- js/index.js
+++ js/index.js
@@ -1,43 +1,60 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    //username und password aus index html bekommen
-    var username_eingabe = document.getElementById('username').value;
-    var password_eingabe = document.getElementById('password').value;
-  
-    
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var user_email = document.querySelector('input[type="text"]').value;
+  var user_password = document.querySelector('input[type="password"]').value;
+
+  if (user_email === '') {
+    showError('Email can\'t be blank');
+  } else if (user_password === '') {
+    showError('Password can\'t be blank');
+  } else {
+
+    console.log('Email:', user_email);
+    console.log('Password:', user_password);
 
     const data = {
-      username: username_eingabe,
-      password: password_eingabe
+      username: user_email,
+      password: user_password
     };
 
     console.log(data);
 
-
     fetch('https://iotplantcare-e4290a52c869.herokuapp.com/checkPW', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'api_key_for_tempMeasuring'
-      },
-      body: JSON.stringify(data)
-    })
-    .then(response => {
-      if (response.ok) {
-        console.log('grant access');
-        alert('Login successful');
-      window.location.href = "dashboard.html";
-      } else {
-        console.log('Error:', response.statusText);
-        alert('Invalid username or password');
-      }
-    })
-    .catch(error => {
-      console.log('Error:', error);
-    });
-    
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'api_key_for_tempMeasuring'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('grant access');
+      alert('Login successful');
+    window.location.href = "dashboard.html";
+    } else {
+      console.log('Error:', response.statusText);
+      alert('Invalid username or password');
+    }
+  })
+  .catch(error => {
+    console.log('Error:', error);
   });
+  
 
 
-  
-  
+
+
+
+  }
+});
+
+// Funktion zum Anzeigen von Fehlermeldungen
+function showError(errorMessage) {
+  var errorText = document.querySelector('.error-txt');
+  errorText.textContent = errorMessage;
+  errorText.style.display = 'block';
+}
+
+
